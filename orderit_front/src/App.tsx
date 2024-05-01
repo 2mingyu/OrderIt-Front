42d10d0026534f37b4b9a8ede340269c981f33f9
mingyu <tnt01010@naver.com>
2024-05-01 12:50:32 +0900

    GlobalStyle

--- orderit_front/src/App.tsx
+++ orderit_front/src/App.tsx
@@ -1,16 +1,25 @@
 import { BrowserRouter, Routes, Route  } from 'react-router-dom';
-
+import { createGlobalStyle } from 'styled-components';
 import HomePage from './pages/HomePage';
 import OrderPage from './pages/OrderPage';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/order" element={<OrderPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/order" element={<OrderPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
